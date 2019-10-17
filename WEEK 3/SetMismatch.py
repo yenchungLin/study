8661f717a4e20420f656704667149647c2195555
yulin <yulin2306@gmail.com>
2019-10-17 08:02:51 +0800

    Create SetMismatch.py

---
+++ WEEK 3/SetMismatch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        ex_list = [i for i in range(1,len(nums))]
+        ex_set = set(ex_list)
+        num_set = set(nums)
+        mis = ex_set - num_set
+        
+        rep = []
+        for i in range(1,len(nums)-1):
+            if nums[i] == nums[i-1]:
+                rep.append(nums[i])
+        rep_set = set(rep)
+        
+        result = mis|rep_set
+        return list(result)
+ 
