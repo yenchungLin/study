cd5526245aba349c0c1976b1d29d25895a379b0f
yulin <yulin2306@gmail.com>
2019-11-01 11:28:24 +0800

    Delete linked_list.py

--- WEEK 1/linked_list.py
+++
@@ -1,8 +0,0 @@
-"""Design your implementation of the linked list. You can choose to use the singly linked list or the doubly linked list. A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node. If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed."""
-class MyLinkedList:
-        """
-        Initialize your data structure here.
-        """
-    def __init__(self):
-        self.val = val
-        self.next = None
