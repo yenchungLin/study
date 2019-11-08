91a5258a808aeb30fa8428b07cf01c2ad280f119
yenchungLin <55729350+yenchungLin@users.noreply.github.com>
2019-11-08 08:18:48 +0800

    Update merge_sort_06170130.py

--- homework助教請開/HW2/merge_sort_06170130.py
+++ homework助教請開/HW2/merge_sort_06170130.py
@@ -262,87 +262,171 @@ def merge(left,right):
         m = m+1
     return M
 
-#merge_sort
-def merge_sort(a):
-    #分組
-    left = [] #前半部分
-    right = [] #後半部分
-    
-    #當資料為一個或是空值時，直接回傳
-    if len(a) <= 1:
-        return a
-    
-    #當資料為兩個以上時，進行分群
-    else:
-        
-        #若資料為奇數時，前半部分多存中間數
-        if len(a)%2 == 1:
-            #print("test1")
-            for i in range(0,len(a)):
-                #前半部分
-                if i <= len(a)//2:
-                    left.append(a[i])
-                #後半部分
-                else:
-                    right.append(a[i])
-                #print("left = ",left,"right = ",right)
-        #當資料為偶數時，直接對分
-        else:
-            #print("test2")
-            for i in range(0,len(a)):
-                #前半部分
-                if i <= len(a)//2 -1:
-                    left.append(a[i])
-                #後半部分
-                else:
-                    right.append(a[i])
-                #print("left = ",left,"right = ",right)
-                
-        #重複執行，進行分組
-        left = merge_sort(left)
-        #print("final_left = ",left)
-        right = merge_sort(right)
-        #print("final_right = ",right)
-        #print(final_left,final_right)
-        return merge
-    
-def merge(left,right):
-    M = [0]*(len(left)+len(right)) #新存放數列的地方
-    m = 0
-    i = 0 #left index的起始值
-    j = 0 #right index的起始值
+class Solutuin(object):
+    def merge_sort(self,i,left,right):
+        #分組
+        left = [] #前半部分
+        right = [] #後半部分
 
-    #若left與right都還有值
-    while i < len(left) and j < len(right):
-        #left值小於等於right值，則增加left到M中
-        if left[i] <= right[j]:
-            print("test1-1")            
+        #當資料為一個或是空值時，直接回傳
+        if len(self) <= 1:
+            return self
+
+        #當資料為兩個以上時，進行分群
+        else:
+
+            #若資料為奇數時，前半部分多存中間數
+            if len(self)%2 == 1:
+                #print("test1")
+                for i in range(0,len(self)):
+                    #前半部分
+                    if i <= len(self)//2:
+                        left.append(self[i])
+                    #後半部分
+                    else:
+                        right.append(self[i])
+                    #print("left = ",left,"right = ",right)
+            #當資料為偶數時，直接對分
+            else:
+                #print("test2")
+                for i in range(0,len(self)):
+                    #前半部分
+                    if i <= len(self)//2 -1:
+                        left.append(self[i])
+                    #後半部分
+                    else:
+                        right.append(self[i])
+                    #print("left = ",left,"right = ",right)
+
+            #重複執行，進行分組
+            left = merge_sort(left)
+            #print("final_left = ",left)
+            right = merge_sort(right)
+            #print("final_right = ",right)
+            #print(final_left,final_right)
+            return merge
+
+    def merge(left,right):
+        M = [0]*(len(left)+len(right)) #新存放數列的地方
+        m = 0
+        i = 0 #left index的起始值
+        j = 0 #right index的起始值
+
+        #若left與right都還有值
+        while i < len(left) and j < len(right):
+            #left值小於等於right值，則增加left到M中
+            if left[i] <= right[j]:
+                print("test1-1")            
+                M[m] = left[i]
+                i = i+1
+                m = m+1
+
+            #left值大於right值，則增加rigjt到M中
+            else:
+                print("test1-2")
+                print("right[j] = ",right[j])
+                print("m=",m)
+                M[m] = right[j]
+                j = j+1
+                m = m+1
+
+        #若left還有值，但是right沒有，則直接增加left進M
+        while i < len(left) and j>= len(right):
+            print("test2")
             M[m] = left[i]
             i = i+1
             m = m+1
-            
-        #left值大於right值，則增加rigjt到M中
-        else:
-            print("test1-2")
-            print("right[j] = ",right[j])
-            print("m=",m)
+        #若right還有值，但是left沒有，則直接增加right進M
+        while j < len(right) and i >= len(left):
+            print("test3")
             M[m] = right[j]
             j = j+1
             m = m+1
-            
-    #若left還有值，但是right沒有，則直接增加left進M
-    while i < len(left) and j>= len(right):
-        print("test2")
-        M[m] = left[i]
-        i = i+1
-        m = m+1
-    #若right還有值，但是left沒有，則直接增加right進M
-    while j < len(right) and i >= len(left):
-        print("test3")
-        M[m] = right[j]
-        j = j+1
-        m = m+1
-    return M
+        return M
+
+    #merge_sort執行地方
+    def merge_sort(a):
+        #分組
+        left = [] #前半部分
+        right = [] #後半部分
+
+        #當資料為一個或是空值時，直接回傳
+        if len(a) <= 1:
+            return a
+
+        #當資料為兩個以上時，進行分群
+        else:
+
+            #若資料為奇數時，前半部分多存中間數
+            if len(a)%2 == 1:
+                #print("test1")
+                for i in range(0,len(a)):
+                    #前半部分
+                    if i <= len(a)//2:
+                        left.append(a[i])
+                    #後半部分
+                    else:
+                        right.append(a[i])
+                    #print("left = ",left,"right = ",right)
+            #當資料為偶數時，直接對分
+            else:
+                #print("test2")
+                for i in range(0,len(a)):
+                    #前半部分
+                    if i <= len(a)//2 -1:
+                        left.append(a[i])
+                    #後半部分
+                    else:
+                        right.append(a[i])
+                    #print("left = ",left,"right = ",right)
+
+            #重複執行，進行分組
+            left = merge_sort(left)
+            #print("final_left = ",left)
+            right = merge_sort(right)
+            #print("final_right = ",right)
+            #print(final_left,final_right)
+            return merge
+
+    def merge(left,right):
+        M = [0]*(len(left)+len(right)) #新存放數列的地方
+        m = 0
+        i = 0 #left index的起始值
+        j = 0 #right index的起始值
+
+        #若left與right都還有值
+        while i < len(left) and j < len(right):
+            #left值小於等於right值，則增加left到M中
+            if left[i] <= right[j]:
+                print("test1-1")            
+                M[m] = left[i]
+                i = i+1
+                m = m+1
+
+            #left值大於right值，則增加rigjt到M中
+            else:
+                print("test1-2")
+                print("right[j] = ",right[j])
+                print("m=",m)
+                M[m] = right[j]
+                j = j+1
+                m = m+1
+
+        #若left還有值，但是right沒有，則直接增加left進M
+        while i < len(left) and j>= len(right):
+            print("test2")
+            M[m] = left[i]
+            i = i+1
+            m = m+1
+        #若right還有值，但是left沒有，則直接增加right進M
+        while j < len(right) and i >= len(left):
+            print("test3")
+            M[m] = right[j]
+            j = j+1
+            m = m+1
+        return M
     
+Output = Solution().merge_sort()
+Output
 
 #merge_sort的處理為0(n log n)，處理的穩度度比quick_sort高，因為不售基準點的好壞影響。
