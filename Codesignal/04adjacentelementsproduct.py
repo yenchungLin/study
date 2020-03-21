64edc10b6216273321bf178661db27c231836015
yulin <yulin2306@gmail.com>
2020-03-21 13:48:10 +0800

    Delete 04adjacentelementsproduct.py

--- Codesignal/04adjacentelementsproduct.py
+++
@@ -1,8 +0,0 @@
-"""
-Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
-"""
-def adjacentElementsProduct(inputArray):
-    large = []
-    for i in range(0, len(inputArray)-1):
-        large.append(inputArray[i]*inputArray[i+1])
-    return max(large)
